fea0417cba106f348e3e9fc9f8d5f1372dc3df46
lajos <lajos.zalan@student.ms.sapientia.ro>
2022-02-17 15:12:27 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/proba.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proba.dir/main.c.obj"
+  "libproba.dll.a"
+  "proba.exe"
+  "proba.exe.manifest"
+  "proba.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/proba.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
